dafa9b36c9d23e3d97a47fa8a36bbb4ab914fabc
phhuongthao <npthao808@gmail.com>
2025-05-21 15:47:40 +0700

    create project

---
+++ src/test/java/com/junit1/JUnit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
